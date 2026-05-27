68a798e07b01e47d9b479f56b9522459c1caeefa
visal <sokchamraeunid@gmail.com>
2026-05-28 00:06:04 +0700

    work test

--- backend/config/broadcasting.php
+++ backend/config/broadcasting.php
@@ -60,7 +60,7 @@ return [
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'verify' => filter_var(env('PUSHER_SSL_VERIFY', true), FILTER_VALIDATE_BOOLEAN),
             ],
         ],
 
