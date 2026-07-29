48613b32b42417d2cb68197e37a880e33cd5b6e4
visal <sokchamraeunid@gmail.com>
2026-07-29 12:29:59 +0700

    add product

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
-            // ProductSeeder::class,
+            ProductSeeder::class,
             IngredientAndRecipeSeeder::class,
         ]);
 
